6866baea74c303ff87419234ce97701e5990c1c0
HuYanQi0722 <xiaoxiaohyq@126.com>
2016-06-27 19:08:18 +0800

    Update cart.h

--- pos1/cart.h
+++ pos1/cart.h
@@ -1,10 +1,15 @@
+#pragma once
 #include "stdafx.h"
 
-class ShoppingCart{
-	list<Item> cart;
+class ShoppingCart {
+	vector<Item> cart;
 public:
 	void add(Item it)
 	{
 		cart.push_back(it);
+	}
+
+	vector<Item> getItems() {
+		return cart;
 	}
 };
