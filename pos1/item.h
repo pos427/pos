8094a4d0ef0bdebbf3d0ce639204fc611274c36f
melonfay <13051180958@163.com>
2016-06-27 18:48:29 +0800

    Change something

--- pos1/item.h
+++ pos1/item.h
@@ -1,71 +1,74 @@
+#pragma once
 #include "stdafx.h"
 
 class Item
 {
-private :
+private:
 	string barcode;
 	string name;
 	string unit;
 	float price;
 	double discount;
-public :
+public:
 	Item() {
-    }
+	}
 
-    Item(string barcode, string name, string unit, double price) {
+	Item(string barcode, string name, string unit, double price) {
 
-        this->setBarcode(barcode);
-        this->setName(name);
-        this->setUnit(unit);
-        this->setPrice(price);
+		this->setBarcode(barcode);
+		this->setName(name);
+		this->setUnit(unit);
+		this->setPrice(price);
 		this->setDiscount(1.00);
-    }
+	}
 
 	Item(string barcode, string name, string unit, double price, double discount) {
-        this->setBarcode(barcode);
-        this->setName(name);
-        this->setUnit(unit);
-        this->setPrice(price);
-        this->setDiscount(discount);
-    }
+		this->setBarcode(barcode);
+		this->setName(name);
+		this->setUnit(unit);
+		this->setPrice(price);
+		this->setDiscount(discount);
+	}
 
-    string getName() {
-        return name;
-    }
+	string getName() {
+		return name;
+	}
 
-    string getUnit() {
-        return unit;
-    }
+	string getUnit() {
+		return unit;
+	}
 
-    double getPrice() {
-        return price;
-    }
+	double getPrice() {
+		return price;
+	}
 
-    string getBarcode() { return barcode; }
+	string getBarcode() { 
+		return barcode; 
+	}
 
-    double getDiscount() {
-        if (discount == 0.00)
-            return 1.00;
-        return discount;
-    }
+	double getDiscount() {
+		if (discount == 0.00)
+			return 1.00;
+		return discount;
+	}
 
-    void setBarcode(string barcode) {
-        this->barcode = barcode;
-    }
+	void setBarcode(string barcode) {
+		this->barcode = barcode;
+	}
 
-    void setName(string name) {
-        this->name = name;
-    }
+	void setName(string name) {
+		this->name = name;
+	}
 
-    void setUnit(string unit) {
-        this->unit = unit;
-    }
+	void setUnit(string unit) {
+		this->unit = unit;
+	}
 
-    void setPrice(double price) {
-        this->price = price;
-    }
+	void setPrice(double price) {
+		this->price = price;
+	}
 
-    void setDiscount(double discount) {
-        this->discount = discount;
-    }
+	void setDiscount(double discount) {
+		this->discount = discount;
+	}
 };
