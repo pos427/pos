d82c585a4ad3c0b1b08a1ff29469a1d13058c465
melonfay <13051180958@163.com>
2016-06-30 23:29:21 +0800

    update for 5,6

--- pos1/cart.h
+++ pos1/cart.h
@@ -1,9 +1,23 @@
 #pragma once
 #include "stdafx.h"
+#include "user.h"
 
 class ShoppingCart {
 	vector<Item> cart;
+	User user;
 public:
+    ShoppingCart(){
+
+    }
+
+    ShoppingCart(User user){
+        this->user = user;
+    }
+
+	User getUser(){
+        return this->user;
+    }
+
 	void add(Item it)
 	{
 		cart.push_back(it);
